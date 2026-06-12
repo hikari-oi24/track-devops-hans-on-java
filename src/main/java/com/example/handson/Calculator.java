f2e050e1a5147fbebd4eeea64e554c998d575507
hikari-oi24 <hikari.oi@tcs.com>
2026-06-12 13:39:06 +0900

    Update Calculator.java

--- src/main/java/com/example/handson/Calculator.java
+++ src/main/java/com/example/handson/Calculator.java
@@ -1,10 +1,13 @@
+
 package com.example.handson;
+
 public class Calculator {
-public double add(double a, double b) {
-return a + b;
-}
-public double add(double a, double b, double c) {
-return a + b + c;
-Java
-}
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double add(double a, double b, double c) {
+        return a + b + c;
+    }
 }
