c24b261f821d35badc4733c3603ec4684069a783
hikari-oi24 <hikari.oi@tcs.com>
2026-06-12 14:16:29 +0900

    Update Calculator.java

--- src/main/java/com/example/handson/Calculator.java
+++ src/main/java/com/example/handson/Calculator.java
@@ -1,18 +1,16 @@
+
 package com.example.handson;
+
 public class Calculator {
-public double add(double a, double b) {
-return a + b;
-}
-Java
-public double add(double a, double b, double c) {
-validateRange(a);
-validateRange(b);
-validateRange(c);
-return a + b + c;
-}
-private void validateRange(double value) {
-if (value < 0 || value > 10) {
-throw new IllegalArgumentException("value must be between 0 and 10");
-}
-}
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double add(double a, double b, double c) {
+        if (a < 0 || b > 10 || c > 10) {
+            throw new IllegalArgumentException();
+        }
+        return a + b + c;
+    }
 }
